24aa2345826616a4f956bb3e3b9db8820ec447a9
C678Sue <2685781399@qq.com>
2024-09-24 12:27:45 +0800

    commit 5
    Make home page but not completed

--- A2.js
+++ A2.js
@@ -1,3 +1,10 @@
-// script.js
+document.addEventListener("DOMContentLoaded", () => {
+    const fundraiserList = document.getElementById('fundraiser-list');
 
-const colorButton
+    // 示例筹款活动数据
+    const fundraisers = [
+        {
+            id: 1,
+            organiser: 'Jackson',
+            caption: "Help The Jackson's Rebuild After Flood",
+            targetFunding: 10000
