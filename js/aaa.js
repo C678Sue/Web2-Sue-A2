b8b108a07b32680e84ab48b2fe24351e96e06375
C678Sue <2685781399@qq.com>
2024-09-28 15:33:57 +0800

    Beautify the page, modify the content

--- js/aaa.js
+++ js/aaa.js
@@ -1,4 +1,4 @@
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     const welcomeMessage = document.getElementById('welcome');
     const welcomeSection = document.querySelector('.welcome-section');
     if (window.scrollY > 100) {
@@ -14,59 +14,56 @@ window.addEventListener('scroll', function() {
 });
 
 const navItems = document.querySelectorAll('.nav-item');
-        navItems.forEach(item => {
-            item.addEventListener('click', () => {
-                navItems.forEach(nav => nav.classList.remove('.nav-item.active'));
-                item.classList.add('.nav-item.active');
-            });
-        });
 
-        // 保持Home按钮在页面加载时为active状态
-        document.getElementById('home').classList.add('.nav-item.active');
-        document.getElementById('search').classList.add('.nav-item.active');
- 
- //  活动数据
- let list = []
- //  获取所有活动
- function getActiveList(){
-     $.get('http://localhost:3000/api/all', function(data, status) {
-          console.log(data);
-          list = data;
-          showList()
-     });
-
- }
+navItems.forEach(item => {
+    item.addEventListener('click', () => {
+        navItems.forEach(nav => nav.classList.remove('active'));
+        item.classList.add('active');
+    });
+});
 
 
- getActiveList()
- //  展示活动列表
-function showList(){
- let html = '';
+//  活动数据
+let list = []
+//  获取所有活动
+function getActiveList() {
+    $.get('http://localhost:3000/api/all', function (data, status) {
+        console.log(data);
+        list = data;
+        showList()
+    });
 
- for(let i=0;i<list.length;i++){
-     let temp =`<div class="active-info" id='active${i+1}'>
+}
+
+
+getActiveList()
+//  展示活动列表
+function showList() {
+    let html = '';
+
+    for (let i = 0; i < list.length; i++) {
+        let temp = `<div class="active-info" id='active${i + 1}'>
            <div class="right">
              <img class="active-img"
              src="http://gips2.baidu.com/it/u=195724436,3554684702&fm=3028&app=3028&f=JPEG&fmt=auto?w=1280&h=960"
              alt="">
          </div>
          <div class="left">
+         <div class="left-item"><div>Caption：</div><div class="line">${list[i].TITLE}</div></div>
              <div class="left-item"><div>ID：</div><div class="line">${list[i].CATEGORY_ID}</div></div>
-             <div class="left-item"><div>组织者：</div><div class="line">${list[i].ORGANIZER}</div></div>
-             <div class="left-item"><div>标题：</div><div class="line">${list[i].TITLE}</div></div>
-             
-             <div class="left-item"><div>目标资金：</div><div class="line">${list[i].TARGET_FUNDING}</div></div>
-             <div class="left-item"><div>当前资金：</div><div class="line">${list[i].CURRENT_FUNDING}</div></div>
-             <div class="left-item"><div>城市：</div><div class="line">${list[i].CITY}</div></div>
-             <div class="left-item"><div>类型：</div><div class="line">${list[i].NAME}</div></div>
+             <div class="left-item"><div>Organiser：</div><div class="line">${list[i].ORGANIZER}</div></div>
+             <div class="left-item"><div>Target funding：</div><div class="line">${list[i].TARGET_FUNDING}</div></div>
+             <div class="left-item"><div>Current funding：</div><div class="line">${list[i].CURRENT_FUNDING}</div></div>
+             <div class="left-item"><div>City：</div><div class="line">${list[i].CITY}</div></div>
+             <div class="left-item"><div>Category：</div><div class="line">${list[i].NAME}</div></div>
          </div>
        
      </div>`
-     html += temp
- }
- $(".list").html(html)
+        html += temp
+    }
+    $(".list").html(html)
 }
 //   跳转到搜索页面
-function toSearch(){
- window.location.href = 'Search fundraisers.html'
+function toSearch() {
+    window.location.href = 'Search fundraisers.html'
 }
